45e72e6a3d52a2c53698de6474a158b535333df1
Simon Eisenmann <simon@longsleep.org>
2015-05-11 23:33:16 +0200

    Added example updater script.

---
+++ scripts/mydyndnsrv-update.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Simple updater script for mydyndnsvr running on your own server. Change the
+# HOST variable below to point to your server. Tokens go as first argument
+# when calling this script.
+HOST="dns.orr.pw"
+
+# Token is passed as first argument.
+TOKEN="$1"
+if [ -z "$TOKEN" ]; then
+	echo -e "Usage: mydyndnsrv-update.sh TOKEN\n"
+	exit 1
+fi
+
+# URL's. If you want to validate the certificate, remote the -k.
+CURL="curl -k"
+IP4=`$CURL -4 -s "https://$HOST/update?token=$TOKEN&check" 2>/dev/null`
+IP6=`$CURL -6 -s "https://$HOST/update?token=$TOKEN&check" 2>/dev/null`
+TIME=`date "+%Y/%m/%d %H:%M:%S"`
+
+OLD_IP4=""
+STATUS_IP6=""
+if [ -e /tmp/mydyndnsrv-update4 ]; then
+	OLD_IP4=`cat /tmp/mydyndnsrv-update4`
+fi
+if [ -n "$IP4" ]; then
+	if [ "$IP4" != "$OLD_IP4" ]; then
+		STATUS_IP4=`$CURL -4 -s "https://$HOST/update?token=$TOKEN&myip=$IP4" 2>/dev/null`
+		if [ "$STATUS_IP4" == "accepted" ]; then
+			echo "$TIME - IPv4 update status $STATUS_IP4:$IP4"
+			echo $IP4 > /tmp/mydyndnsrv-update4
+		else
+			echo "$TIME - IPv4 update status failed:$STATUS_IP4"
+		fi
+	fi
+fi
+
+OLD_IP6=""
+STATUS_IP6=""
+if [ -e /tmp/mydyndnsrv-update6 ]; then
+	OLD_IP6=`cat /tmp/mydyndnsrv-update6`
+fi
+if [ -n "$IP6" ]; then
+	if [ "$IP6" != "$OLD_IP6" ]; then
+		STATUS_IP6=`$CURL -6 -s "https://$HOST/update?token=$TOKEN&myip=$IP6" 2>/dev/null`
+		if [ "$STATUS_IP6" == "accepted" ]; then
+			echo "$TIME - IPv6 update status $STATUS_IP6:$IP6"
+			echo $IP6 > /tmp/mydyndnsrv-update6
+		else
+			echo "$TIME - IPv6 update status failed:$STATUS_IP6"
+		fi
+	fi
+fi
